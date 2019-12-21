84f28dc239cc03cac9dfc340cbb53d788372495a
최철환 <hwanyhee@naver.com>
2019-12-21 17:43:14 +0900

    리팩토링 - Header 컴포넌트를 별도의 화일로 분리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import './App.css';
-
-const Header = (props) => {
-  console.log(props);
-  return (
-    <header className="header">
-      <h1 className="h1">{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayer}</span>
-    </header>
-  );
-}
+import {Header} from './components/Header';
 
 const Player = (props) => (
   <div className="player">
