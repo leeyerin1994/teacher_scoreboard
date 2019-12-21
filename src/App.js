9577578642a046c292c90a109402ea9a3c865d04
최철환 <hwanyhee@naver.com>
2019-12-21 17:09:36 +0900

    1주차소스 마이그레이션

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Header = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="header">
+      <h1 className="h1">{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayer}</span>
+    </header>
   );
+}
+
+const Player = (props) => (
+  <div className="player">
+		<span className="player-name">
+			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
+      {props.name}
+		</span>
+    <Counter/>
+  </div>
+);
+
+class Counter extends React.Component {
+  state = {
+    score: 30,
+    a: 1
+  }
+
+  incrementScore = () => {
+    console.log('incrementScore: ', this);
+    // this.state.score += 1;
+    this.setState(prevState => ({ score: prevState.score + 1 }));
+  }
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> -</button>
+        <span className="counter-score">{this.state.score}</span>
+        {/*이벤트 핸들러 우측에는 함수 선언문이 와야 한다.*/}
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    players: [
+      { name: 'LDK', id: 1},
+      { name: 'PARK', id: 2},
+      { name: 'KIM', id: 3},
+      { name: 'LEE', id: 4}
+    ]
+  }
+  handleRemovePlayer = (id) => {
+    console.log('handleRemovePlayer: ', id);
+    this.setState(prevState => {
+      const players = prevState.players.filter(player => player.id !== id);
+      return {
+        players: players
+      }
+    })
+  }
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header title="My Scoreboard" totalPlayer={10 + 1}></Header>
+
+        {
+          this.state.players.map(player => (
+              <Player name={player.name} key={player.id} id={player.id}
+                      removePlayer={this.handleRemovePlayer}></Player>
+            )
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
