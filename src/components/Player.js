9c43fbd18432a7c74396e15ba63b1d50d376ceee
최철환 <hwanyhee@naver.com>
2019-12-28 18:24:27 +0900

    Player 컴포넌트 리덕스적용 - 리덕스 useDispatch hooks사용하여 자식과 부모간의 통신에 적용

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,22 +1,19 @@
 import React from "react";
 import Counter from "./Counter";
 import {removePlayer} from "../redux/actions";
-import {connect} from "react-redux";
+import {useDispatch} from "react-redux";
 
-export const Player = (props) => (
-  <div className="player">
-		<span className="player-name">
-			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
-      {props.name}
-		</span>
-    <Counter id={props.id} score={props.score} />
-  </div>
-);
-
-const mapActionToProps = (dispatch) => ({
-	// 왼쪼은 props, 오른쪽은 (액션을 디스패치하는)펑션
-	removePlayer: (id) => dispatch(removePlayer(id))
-})
-
+export const Player = (props) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="player">
+		  <span className="player-name">
+        <button className="remove-player" onClick={() => dispatch(removePlayer(props.id))}> x </button>
+        {props.name}
+      </span>
+      <Counter id={props.id} score={props.score} />
+    </div>
+  );
+}
 // 커링 펑션, HoC 컴포넌튼
-export default connect(null, mapActionToProps)(Player);
+export default Player;
