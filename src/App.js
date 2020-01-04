de15d88b6419d403ad09758ab53eab6701771dc2
최철환 <hwanyhee@naver.com>
2020-01-04 11:39:18 +0900

    highscore 구하기 - 부모 컴포넌트에서 isHighScore 속성구하기

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Player from "./components/Player";
 import AddPlayerForm from "./components/AddPlayerForm";
 import {useSelector} from "react-redux";
 import {CustomPlayer} from "./components/CustomPlayer";
+import _ from 'lodash';
 
 const App = () => {
   const players = useSelector(state => state.playerReducer.players);
+
+  // 최고 점수를 리턴
+  const getHighScore  = () => {
+    return  _.maxBy(players, 'score');
+  }
 
   return (
     <div className="scoreboard">
@@ -15,7 +21,8 @@ const App = () => {
 
       {
         players.map(player => (
-            <CustomPlayer name={player.name} key={player.id} id={player.id} score={player.score}>
+            <CustomPlayer name={player.name} key={player.id} id={player.id} score={player.score}
+              isHighScore={player.score === getHighScore()}>
             </CustomPlayer>
           )
         )
