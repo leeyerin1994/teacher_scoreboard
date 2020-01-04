91f29575f3772848e9b8af1a0422005605883c03
최철환 <hwanyhee@naver.com>
2020-01-04 13:06:54 +0900

    AddPlayerForm 컴포넌트 - useDispatch hooks 적용

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
 import {addPlayer} from "../redux/actions";
-import {connect} from "react-redux";
+import {connect, useDispatch} from "react-redux";
 
 const AddPlayerForm = () => {
   const [value, setValue] = useState('');
+  const dispatch = useDispatch();
 
   const submit = (e) => {
     // form의 기본 이벤트 막기
     e.preventDefault();
-    this.props.addPlayer(this.state.value);
-    this.setState({value: ''})
+    dispatch(addPlayer(value));
+    setValue('');
   }
 
   return (
@@ -21,12 +22,4 @@ const AddPlayerForm = () => {
   );
 }
 
-// 자식 => 부모 통신: 펑션을 props로 전달
-// action dispatch : (액션을 디스패치하는) 펑션을 props 로 매핑
-const mapActionToProps = (dispatch) => ({
-  // 왼쪼은 props, 오른쪽은 (액션을 디스패치하는)펑션
-  addPlayer: (name) => dispatch(addPlayer(name))
-})
-
-// 커링 펑션, HoC 컴포넌튼
-export default connect(null, mapActionToProps)(AddPlayerForm);
+export default AddPlayerForm;
