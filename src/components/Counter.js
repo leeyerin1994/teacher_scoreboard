398ca1f35756f14b227cc585d885eefd2b59fd78
최철환 <hwanyhee@naver.com>
2020-01-04 10:21:29 +0900

    Counter 컴포넌트 - useDispatch hooks 적용

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,20 @@
 import React from "react";
 import {changeScore} from "../redux/actions";
-import {connect} from "react-redux";
+import {connect, useDispatch} from "react-redux";
 
-export class Counter extends React.Component {
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement"
-                onClick={() => this.props.changeScore(this.props.id, -1)}> -</button>
-        <span className="counter-score">{this.props.score}</span>
-        {/*이벤트 핸들러 우측에는 함수 선언문이 와야 한다.*/}
-        <button className="counter-action increment"
-                onClick={() => this.props.changeScore(this.props.id, 1)}> + </button>
-      </div>
-    );
-  }
+const Counter = (props) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"
+              onClick={() => dispatch(changeScore(props.id, -1))}> -</button>
+      <span className="counter-score">{this.props.score}</span>
+      {/*이벤트 핸들러 우측에는 함수 선언문이 와야 한다.*/}
+      <button className="counter-action increment"
+              onClick={() => dispatch(changeScore(props.id, 1))}> + </button>
+    </div>
+  );
 }
 
-const mapActionToProps = (dispatch) => ({
-  // 왼쪼은 props, 오른쪽은 (액션을 디스패치하는)펑션
-  changeScore: (id, delta) => dispatch(changeScore(id, delta))
-})
-
-// 커링 펑션, HoC 컴포넌튼
-export default connect(null, mapActionToProps)(Counter);
+export default Counter;
