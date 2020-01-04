dcd1b1871f8623bbc28499c5bb06c9ac97168269
최철환 <hwanyhee@naver.com>
2020-01-04 10:06:34 +0900

    App 컴포넌트 - useSelector 리덕스 hooks 적용

--- src/App.js
+++ src/App.js
@@ -3,45 +3,25 @@ import './App.css';
 import {Header} from './components/Header';
 import Player from "./components/Player";
 import AddPlayerForm from "./components/AddPlayerForm";
-import {connect} from "react-redux";
+import {useSelector} from "react-redux";
 
-let maxId = 4;
+const App = () => {
+  const players = useSelector(state => state.playerReducer.players);
 
-class App extends React.Component {
+  return (
+    <div className="scoreboard">
+      <Header title="My Scoreboard" players={players}></Header>
 
-/*  handleRemovePlayer = (id) => {
-    console.log('handleRemovePlayer: ', id);
-    this.setState(prevState => {
-      const players = prevState.players.filter(player => player.id !== id);
-      return {
-        players: players
-      }
-    })
-  }*/
-  render() {
-    return (
-      <div className="scoreboard">
-        <Header title="My Scoreboard" players={this.props.players}></Header>
-
-        {
-          this.props.players.map(player => (
-              <Player name={player.name} key={player.id} id={player.id} score={player.score}>
-              </Player>
-            )
+      {
+        players.map(player => (
+            <Player name={player.name} key={player.id} id={player.id} score={player.score}>
+            </Player>
           )
-        }
-        <AddPlayerForm></AddPlayerForm>
-      </div>
-    );
-  }
+        )
+      }
+      <AddPlayerForm></AddPlayerForm>
+    </div>
+  );
 }
 
-// subscribe: store의 state => 나의, props로 매핑
-// 부모 => 자식 통신
-const mapStateToProps = (state) => ({
-  // 왼쪽이 props, 오른쪽이 store의 state
-  players: state.playerReducer.players
-})
-
-// 커링 펑션, HoC 컴포넌튼
-export default connect(mapStateToProps)(App);
+export default App;
