611b915b0b80c8d7a1b147bcaa627582d89b4a3a
최철환 <hwanyhee@naver.com>
2020-01-04 10:33:08 +0900

    .

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,7 +9,7 @@ const Counter = (props) => {
     <div className="counter">
       <button className="counter-action decrement"
               onClick={() => dispatch(changeScore(props.id, -1))}> -</button>
-      <span className="counter-score">{this.props.score}</span>
+      <span className="counter-score">{props.score}</span>
       {/*이벤트 핸들러 우측에는 함수 선언문이 와야 한다.*/}
       <button className="counter-action increment"
               onClick={() => dispatch(changeScore(props.id, 1))}> + </button>
