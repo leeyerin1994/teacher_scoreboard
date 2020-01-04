283cf8e81153c032b79ce0c3c72fb45a53a4c804
최철환 <hwanyhee@naver.com>
2020-01-04 18:20:00 +0900

    목록 가져오기

--- src/pages/Heroes.js
+++ src/pages/Heroes.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 export const Heroes = (props) => {
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    getHeroes()
+  }, []);
+
+  const getHeroes = () => {
+    axios.get('http://ec2-15-164-134-124.ap-northeast-2.compute.amazonaws.com:8000/api/user/heroes?start_index=0&page_size=5')
+      .then(res => {
+        setHeroes(res.data.data);
+        console.log(heroes);
+      });
+  }
+
   return (
-    <div>Heroes works!!</div>
+    <ul>
+      {heroes.map(hero => (
+        <li key={hero.id}>
+          <img src={hero.photo} alt={hero.name} />
+          <span>{hero.name}</span>
+        </li>
+      ))
+      }
+    </ul>
   );
 }
